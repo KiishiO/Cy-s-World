4b7df1743ea92ac17e9c763e5be3dddfb7bb59b1
Jrock25 <jayden.boss2025@gmail.com>
2025-02-04 00:59:06 -0600

    HelloWrld/People

---
+++ Experiments/Jayden/Experiment1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -0,0 +1,35 @@
+package coms309;
+
+import com.sun.net.httpserver.Headers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.servlet.function.ServerRequest;
+
+
+@RestController
+class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello and welcome to COMS 309";
+    }
+
+    @GetMapping("/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello and welcome to COMS 309: " + name;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
