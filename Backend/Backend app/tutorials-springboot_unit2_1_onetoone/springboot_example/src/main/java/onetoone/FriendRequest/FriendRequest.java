1d4ada5911941324c2c4ea4714938b9b6f6a93d7
Sonia <soniap@iastate.edu>
2025-03-03 14:48:06 -0600

    new friendRequest info and updated main

---
+++ Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/FriendRequest/FriendRequest.java
@@ -0,0 +1,29 @@
+package onetoone.FriendRequest;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+import onetoone.Persons.Person;
+
+@Entity
+@Table(name= "FriendRequest")
+public class FriendRequest {
+
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
