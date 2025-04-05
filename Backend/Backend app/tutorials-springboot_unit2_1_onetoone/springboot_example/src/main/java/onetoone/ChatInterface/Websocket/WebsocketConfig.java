e715dc1391e5013c826f2da4e23c72b440beeb9a
Sonia <soniap@iastate.edu>
2025-04-04 20:44:25 -0500

    chat interface

---
+++ Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/ChatInterface/Websocket/WebsocketConfig.java
@@ -0,0 +1,17 @@
+package onetoone.ChatInterface.Websocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author Sonia Patil
+ *
+ */
+@Configuration
+public class WebsocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
