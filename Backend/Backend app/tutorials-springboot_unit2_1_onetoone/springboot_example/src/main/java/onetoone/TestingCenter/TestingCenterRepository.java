924693f0b45aa33d120dc09a33a3cf4632d1e446
Sonia-Patil <75759641+Sonia-Patil@users.noreply.github.com>
2025-04-08 11:22:26 -0500

    finished testing center respository

--- Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/TestingCenter/TestingCenterRepository.java
+++ Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/TestingCenter/TestingCenterRepository.java
@@ -1,4 +1,22 @@
 package onetoone.TestingCenter;
 
-public interface TestingCenterRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Sonia Patil
+ */@Repository
+public interface TestingCenterRepository extends JpaRepository<TestingCenter, Integer> {
+
+     //find testing center by name
+    List<TestingCenter> findByTestingCenterName(String name);
+
+    //find by location
+    List<TestingCenter> findByTestingCenterLocation(String location);
+
+    //find by specfic exam
+    List<TestingCenter> findByExam(String examInfo);
+
 }
