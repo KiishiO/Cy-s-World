9cd044d6c7d6d659145dabecaf4a8e45e7545e7b
Sonia-Patil <75759641+Sonia-Patil@users.noreply.github.com>
2025-04-09 14:42:09 -0500

    bookstore and products constructors

--- Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/Bookstore/Bookstore.java
+++ Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/Bookstore/Bookstore.java
@@ -1,4 +1,64 @@
 package onetoone.Bookstore;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sonia Patil
+ */
+@Entity
 public class Bookstore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double location;
+
+    //list to hold the products that are available at the bookstore
+    @OneToMany(mappedBy = "bookstore", cascade = CascadeType.ALL)
+    private List<Products> products;
+
+    public Bookstore() {}
+
+    public Bookstore(String name, double location) {
+        this.name = name;
+        this.location = location;
+        products = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLocation() {
+        return location;
+    }
+
+    public void setLocation(double location) {
+        this.location = location;
+    }
+
+    public List<Products> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Products> products) {
+        this.products = products;
+    }
 }
+
--- Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/Bookstore/Products.java
+++ Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/Bookstore/Products.java
@@ -1,4 +1,60 @@
 package onetoone.Bookstore;
 
+import java.util.List;
+import jakarta.persistence.*;
+
+/**
+ * @author Sonia Patil
+ */
+@Entity
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String item;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "bookstore_id")
+    private Bookstore bookstore;
+
+    public Products() {}
+
+    public Products(String item, double price, Bookstore bookstore) {
+        this.item = item;
+        this.price = price;
+        this.bookstore = bookstore;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Bookstore getBookstore() {
+        return bookstore;
+    }
+
+    public void setBookstore(Bookstore bookstore) {
+        this.bookstore = bookstore;
+    }
 }
