a622727e44011df1694067cf03b778b9764afd57
Sonia-Patil <75759641+Sonia-Patil@users.noreply.github.com>
2025-05-07 09:52:45 -0500

    fixing recursion

--- Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/DiningHall/DiningHall.java
+++ Backend/Backend app/tutorials-springboot_unit2_1_onetoone/springboot_example/src/main/java/onetoone/DiningHall/DiningHall.java
@@ -1,4 +1,5 @@
 package onetoone.DiningHall;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
@@ -18,6 +19,7 @@ public class DiningHall {
 
     //list to hold the menu items at a dining hall
     @OneToMany(mappedBy = "diningHall", cascade = CascadeType.ALL)
+    @JsonManagedReference
     private List<MenuItems> menuItems;
 
     public DiningHall(String name, String location) {
